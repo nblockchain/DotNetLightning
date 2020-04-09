0d7ca38b442e61f45d39d0028a7c572fb2d09f73
Andrew Cann <shum@canndrew.org>
2020-04-09 14:22:49 +0800

    Mono-hop unidirectional payments

--- src/DotNetLightning.Infrastructure/ActorManagers/ChannelManager.fs
+++ src/DotNetLightning.Infrastructure/ActorManagers/ChannelManager.fs
@@ -202,6 +202,8 @@ type ChannelManager(log: ILogger<ChannelManager>,
                     return! (this.Actors.[e.NodeId.Value] :> IActor<_>).Put(ChannelCommand.RemoteShutdown m)
                 | :? ClosingSignedMsg as m ->
                     return! (this.Actors.[e.NodeId.Value] :> IActor<_>).Put(ChannelCommand.ApplyClosingSigned m)
+                | :? MonoHopUnidirectionalPaymentMsg as m ->
+                    return! (this.Actors.[e.NodeId.Value] :> IActor<_>).Put(ChannelCommand.ApplyMonoHopUnidirectionalPayment m)
                 | :? UpdateAddHTLCMsg as m ->
                     return! (this.Actors.[e.NodeId.Value] :> IActor<_>).Put(ChannelCommand.ApplyUpdateAddHTLC (m, this.CurrentBlockHeight))
                 | :? UpdateFulfillHTLCMsg as m ->
