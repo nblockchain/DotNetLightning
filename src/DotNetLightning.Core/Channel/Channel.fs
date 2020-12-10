0fcb3d5a69143a8725fbbde2681329cf741cc90e
Andrew Cann <shum@canndrew.org>
2020-12-10 16:24:14 +0800

    Change type (again) of NegotiatingData.ClosingTxPerformed
    This field had type List<Option<ClosingTxPerformed>>, however a None
    could only appear as the final element in this list where it was
    ignored anyway. The program logic is identical if we simply make this
    field a List<ClosingTxPerformed> and never insert a None at the end of
    the list.

--- src/DotNetLightning.Core/Channel/Channel.fs
+++ src/DotNetLightning.Core/Channel/Channel.fs
@@ -974,7 +974,7 @@ module Channel =
                                 RemoteNextCommitInfo = state.RemoteNextCommitInfo
                                 LocalShutdown = localShutdownScriptPubKey
                                 RemoteShutdown = msg.ScriptPubKey
-                                ClosingTxProposed = [ Some {
+                                ClosingTxProposed = [{
                                     ClosingTxProposed.UnsignedTx = closingTx
                                     LocalClosingSigned = closingSignedMsg
                                 }]
@@ -991,7 +991,7 @@ module Channel =
                                 RemoteNextCommitInfo = state.RemoteNextCommitInfo
                                 LocalShutdown = localShutdownScriptPubKey
                                 RemoteShutdown = msg.ScriptPubKey
-                                ClosingTxProposed = [ None ]
+                                ClosingTxProposed = []
                                 MaybeBestUnpublishedTx = None
                             }
                             return [ AcceptedShutdownWhenNoPendingHTLCs(None, nextState) ]
@@ -1042,11 +1042,10 @@ module Channel =
                 let maybeLocalFee =
                     state.ClosingTxProposed
                     |> List.tryHead
-                    |> Option.bind id
                     |> Option.map (fun v -> v.LocalClosingSigned.FeeSatoshis)
                 let areWeInDeal = Some(msg.FeeSatoshis) = maybeLocalFee
                 let hasTooManyNegotiationDone =
-                    (state.ClosingTxProposed |> List.choose (id) |> List.length) >= cs.ChannelOptions.MaxClosingNegotiationIterations
+                    (state.ClosingTxProposed |> List.length) >= cs.ChannelOptions.MaxClosingNegotiationIterations
                 if (areWeInDeal || hasTooManyNegotiationDone) then
                     return!
                         Closing.handleMutualClose
@@ -1054,7 +1053,7 @@ module Channel =
                             { state with MaybeBestUnpublishedTx = Some(finalizedTx) }
                             state.RemoteNextCommitInfo
                 else
-                    let lastLocalClosingFee = state.ClosingTxProposed |> List.tryHead |> Option.bind id |> Option.map (fun txp -> txp.LocalClosingSigned.FeeSatoshis)
+                    let lastLocalClosingFee = state.ClosingTxProposed |> List.tryHead |> Option.map (fun txp -> txp.LocalClosingSigned.FeeSatoshis)
                     let! localF = 
                         match lastLocalClosingFee with
                         | Some v -> Ok v
@@ -1077,7 +1076,7 @@ module Channel =
                     else if (nextClosingFee = msg.FeeSatoshis) then
                         // we have reached on agreement!
                         let closingTxProposed1 =
-                            let newProposed = Some {
+                            let newProposed = {
                                 ClosingTxProposed.UnsignedTx = closingTx
                                 LocalClosingSigned = closingSignedMsg
                             }
@@ -1101,7 +1100,7 @@ module Channel =
                             )
                             |> expectTransactionError
                         let closingTxProposed1 =
-                            let newProposed = Some {
+                            let newProposed = {
                                 ClosingTxProposed.UnsignedTx = closingTx
                                 LocalClosingSigned = closingSignedMsg
                             }
