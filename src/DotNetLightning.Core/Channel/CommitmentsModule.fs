ae083580d78a3f03e2f4925bb3ae0e6065a696af
Afshin Arani <afshinarani79@gmail.com>
2021-06-14 19:01:53 +0430

    Typo: NowKnown -> NotKnown

--- src/DotNetLightning.Core/Channel/CommitmentsModule.fs
+++ src/DotNetLightning.Core/Channel/CommitmentsModule.fs
@@ -176,7 +176,7 @@ module internal Commitments =
                     match cm.OriginChannels.TryGetValue(msg.HTLCId) with
                     | true, origin -> Ok origin
                     | false, _ ->
-                        msg.HTLCId |> htlcOriginNowKnown
+                        msg.HTLCId |> htlcOriginNotKnown
                 let nextC = cm.AddRemoteProposal(msg)
                 return [WeAcceptedFailHTLC(o, htlc, nextC)]
             }
@@ -214,7 +214,7 @@ module internal Commitments =
                         match cm.OriginChannels.TryGetValue(msg.HTLCId) with
                         | true, o -> Ok o
                         | false, _ ->
-                            msg.HTLCId |> htlcOriginNowKnown
+                            msg.HTLCId |> htlcOriginNotKnown
                     let nextC = cm.AddRemoteProposal(msg)
                     return [WeAcceptedFailMalformedHTLC(o, htlc, nextC)]
                 }
