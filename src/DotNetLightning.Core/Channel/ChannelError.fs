b59e519f567bfa79f44ef3d7a9c7b2eb71d499fd
Andrew Cann <shum@canndrew.org>
2021-06-17 16:02:16 +0800

    Make SignCommitment not return an Option
    Previously SignCommitment would return None instead of a
    CommitmentSignedMsg if either:
      (a) There were not updates to sign or
      (b) A revoke-and-ack for a previous commitment was not yet received
    
    Since these cases both indicate a misuse of the API they have been
    changed to errors.

--- src/DotNetLightning.Core/Channel/ChannelError.fs
+++ src/DotNetLightning.Core/Channel/ChannelError.fs
@@ -54,6 +54,8 @@ type ChannelError =
     | CannotCloseChannel of msg: string
     | RemoteProposedHigherFeeThanBaseFee of baseFee: Money * proposedFee: Money
     | RemoteProposedFeeOutOfNegotiatedRange of ourPreviousFee: Money * theirPreviousFee: Money * theirNextFee: Money
+    | NoUpdatesToSign
+    | CannotSignCommitmentBeforeRevocation
     // ---- invalid command ----
     | InvalidOperationAddHTLC of InvalidOperationAddHTLCError
     // -------------------------
@@ -87,6 +89,8 @@ type ChannelError =
         | FundingTxNotGiven _ -> Ignore
         | OnceConfirmedFundingTxHasBecomeUnconfirmed _ -> Close
         | CannotCloseChannel _ -> Ignore
+        | NoUpdatesToSign -> Ignore
+        | CannotSignCommitmentBeforeRevocation -> Ignore
         | InvalidOperationAddHTLC _ -> Ignore
         | RemoteProposedHigherFeeThanBaseFee(_, _) -> Close
         | RemoteProposedFeeOutOfNegotiatedRange(_, _, _) -> Close
@@ -163,6 +167,10 @@ type ChannelError =
             sprintf "Funding tx not given: %s" msg
         | CannotCloseChannel msg ->
             sprintf "Cannot close channel: %s" msg
+        | NoUpdatesToSign ->
+            "No updates to sign"
+        | CannotSignCommitmentBeforeRevocation ->
+            "Cannot sign commitment before previous commitment is revoked"
         | InvalidOperationAddHTLC invalidOperationAddHTLCError ->
             sprintf "Invalid operation (add htlc): %s" invalidOperationAddHTLCError.Message
 
